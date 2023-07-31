3847c7f979bd0fb9741ff64708f8f126cd4f3d3b
Hyunsung Song <song.hyunsung96@gmail.com>
2023-07-31 13:33:35 -0400

    edit get member endpoint

--- routes/member.js
+++ routes/member.js
@@ -5,7 +5,31 @@ let router = express.Router();
 
 router.route("/").get(async (req, res) => {
   memberList = await memberModel.find({});
-  res.json(memberList);
+  memberListDTO = [];
+
+  memberList.forEach(member => {
+    const botRole = "CLOVER BOT";
+    const everyoneRole = "@everyone";
+    let roles = [];
+    let isBot = false;
+
+    member.roles.forEach(role => {
+      if(role !== botRole){
+        if(role !== everyoneRole){
+          roles.push(role);
+        }
+      } else {
+        isBot = true;
+      }
+    })
+
+    if(!isBot){
+      member.roles = roles;
+      memberListDTO.push(member);
+    }
+  });
+
+  res.json(memberListDTO);
 });
 
 module.exports = router
