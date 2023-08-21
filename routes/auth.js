f3764a8be0739bb2ad92709f3eb94f3c55ee9225
Hyunsung Song <song.hyunsung96@gmail.com>
2023-08-21 00:44:02 -0400

    ..

--- routes/auth.js
+++ routes/auth.js
@@ -12,52 +12,68 @@ router.route("/check-200").get((req, res, next) => {
   res.status(200).send("you are logged in");
 })
 
-router.route("/redirect").get((req, res, next) => {
+router.route("/authenticate").get(async (req, res, next) => {
+  const ADMIN_ROLE = "721906462749884427";
+  let isAdmin = false;
   console.log(`The authorization code is: ${req.query.code}`);
   console.log("Now calling discord with authorization code for access token");
 
-  let axiosInstance = axios.create({
-    headers: {
-      "Content-Type": 'application/x-www-form-urlencoded'
-    }
-  })
-
   if(req.query.code){
-    axiosInstance.post('https://discord.com/api/oauth2/token', {
-      client_id: process.env.DISCORD_CLIENT_ID,
-      client_secret: process.env.DISCORD_CLIENT_SECRET,
-      code: req.query.code,
-      grant_type: 'authorization_code',
-      redirect_uri: "http://localhost:8080/auth/redirect",
-      scope: 'identify'
-    }).then(res => {
-      console.log("authorization code was sent");
-      console.log(res.data);
-    }).catch(err => {
-      console.log(err);
-      next(err);
-    })
+    try {
+      let tokenResponse = await axios.post('https://discord.com/api/oauth2/token',
+      {
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_CLIENT_SECRET,
+        code: req.query.code,
+        grant_type: 'authorization_code',
+        redirect_uri: "http://localhost:3000/login/redirect",
+        scope: 'identify'
+      },
+      {
+        headers: {
+          "Content-Type": 'application/x-www-form-urlencoded'
+        }
+      })
+
+      console.log("Now calling discord with access token for user data");
+
+      if(tokenResponse.data && tokenResponse.data.access_token){
+        try {
+          let userResponse = await axios.get('https://discord.com/api/users/@me/guilds/' + process.env.DISCORD_GUILD_ID + "/member", 
+          {
+            headers: {
+              "authorization": `Bearer ${tokenResponse.data.access_token}`
+            }
+          })
+          if(userResponse && userResponse.data){            
+            const nick = userResponse.data.nick;
+            const roles = userResponse.data.roles;
+            
+            for(let role of roles){
+              if(role == ADMIN_ROLE){
+                isAdmin = true;
+                break;
+              }
+            }
+          }
+          if(isAdmin){
+            res.status(200).send("Admin confirmed, set session");
+          } else {
+            res.status(403).send("This user is not an admin, unauthorized access");
+          }
+        } catch(e){
+          console.error(e);
+          next(e);
+        }
+      }
+    } catch(e){
+      // TODO - pass status and error message from axios to handler
+      console.error(e);
+      next(e);
+    }
+  } else {
+    res.status(401).send("code is missing");
   }
 });
-
-router.route("/token").get((req, res, next) => {
-  console.log(`The access token is: ${req.query.access_token}`);
-  console.log("Now calling discord with access token for user data");
-
-  if(req.query.access_token){
-    let axiosInstance = axios.create({
-      headers: {
-        "authorization": `Bearer ${req.query.access_token}`
-      }
-    })
-
-    axiosInstance.get('https://discord.com/api/users/@me/guilds/' + process.env.DISCORD_GUILD_ID + "/member").then(res => {
-      console.log(res.data);
-    }).catch(err => {
-      console.log(err);
-      next(err);
-    });
-  }
-})
 
 module.exports = router
