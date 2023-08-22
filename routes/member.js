ad3c6bab476d143b22bb1a04bd8c167716945e0c
Hyunsung Song <song.hyunsung96@gmail.com>
2023-08-22 00:14:57 -0400

    finish changing

--- routes/member.js
+++ routes/member.js
@@ -5,33 +5,35 @@ let router = express.Router();
 const middleware = require('../middleware/authenticateToken');
 
 router.route("/").get(middleware.authenticateToken, async (req, res) => {
-  console.log("Member endpoint invoked by user: " + req.token.name + " at time: " + new Date());
-  memberList = await memberModel.find({});
-  memberListDTO = [];
-
-  memberList.forEach(member => {
-    const botRole = "CLOVER BOT";
-    const everyoneRole = "@everyone";
-    let roles = [];
-    let isBot = false;
-
-    member.roles.forEach(role => {
-      if(role !== botRole){
-        if(role !== everyoneRole){
-          roles.push(role);
+  if(req.token){
+    console.log("Member endpoint invoked by user: " + req.token.name + " at time: " + new Date());
+    memberList = await memberModel.find({});
+    memberListDTO = [];
+  
+    memberList.forEach(member => {
+      const botRole = "CLOVER BOT";
+      const everyoneRole = "@everyone";
+      let roles = [];
+      let isBot = false;
+  
+      member.roles.forEach(role => {
+        if(role !== botRole){
+          if(role !== everyoneRole){
+            roles.push(role);
+          }
+        } else {
+          isBot = true;
         }
-      } else {
-        isBot = true;
+      })
+  
+      if(!isBot){
+        member.roles = roles;
+        memberListDTO.push(member);
       }
-    })
-
-    if(!isBot){
-      member.roles = roles;
-      memberListDTO.push(member);
-    }
-  });
-
-  res.json(memberListDTO);
+    });
+  
+    res.json(memberListDTO);
+  }
 });
 
 module.exports = router
